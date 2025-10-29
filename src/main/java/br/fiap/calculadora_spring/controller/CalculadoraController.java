5d8718cfdaf8c5053228656b0979007a6fca0523
Gustavo Pereira Marques <gustavoper.marq@gmail.com>
2025-10-29 15:15:31 -0300

    Comentarios

--- src/main/java/br/fiap/calculadora_spring/controller/CalculadoraController.java
+++ src/main/java/br/fiap/calculadora_spring/controller/CalculadoraController.java
@@ -14,24 +14,30 @@ public class CalculadoraController {
 
     public CalculadoraController(CalculadoraService service) {this.service = service; }
 
-    @PostMapping("/calcular")
-    public String calcular(@RequestParam String valor1,
-                           @RequestParam String valor2,
-                           @RequestParam String operacao,
+    @PostMapping("/calcular") //Mapeia este método para a URL ".../calcular"
+    public String calcular(@RequestParam String valor1, //pede valor1
+                           @RequestParam String valor2, //pede valor2
+                           @RequestParam String operacao, //pede qual operação
                            Model model)
     {
-        BigDecimal resultado = null;
-        String erro = "";
+        BigDecimal resultado = null; //criada para guardar o resultado
+        String erro = ""; //variavel para guardar uma mensagem de erro
 
         try{
             resultado = service.calcular(valor1, valor2, operacao);
+            //manda o calculo para service
         } catch (Exception e){
+            //pega um erro e guarda mensagem
             erro = e.getMessage();
         }
+        // No HTML, você usará th:text="${resultado}"
         model.addAttribute("resultado", resultado);
+        // Envia a mensagem de erro (se houver) para o HTML (th:text="${erro}")
         model.addAttribute("erro",erro);
+        // Envia os valores de volta, para que o HTML possa manter os campos preenchidos
         model.addAttribute("valor1",valor1);
         model.addAttribute("valor2",valor2);
+        // (Útil para o th:checked que mantem o que o usuario escolheu)
         model.addAttribute("operacao",operacao);
         return "index";
     }
--- src/main/java/br/fiap/calculadora_spring/service/CalculadoraService.java
+++ src/main/java/br/fiap/calculadora_spring/service/CalculadoraService.java
@@ -9,20 +9,26 @@ import java.math.RoundingMode;
 public class CalculadoraService {
 
     public BigDecimal calcular(String valor1, String valor2, String operacao) {
+        //Big decimal serve para evitar problemas no arredondamento e adiciona métodos que facilitam operações
+        //Tais métodos como add, multiply
         BigDecimal v1 = toBigDecimal(valor1);
         BigDecimal v2 = toBigDecimal(valor2);
 
         return switch(operacao) {
-            case "soma" -> v1.add(v2);
+            case "soma" -> v1.add(v2); //case no MESMO nome do value
             case "subtracao" -> v1.subtract(v2);
             case "multiplicacao" -> v1.multiply(v2);
             case "divisao" -> {
-                if (v2.compareTo(BigDecimal.ZERO) == 0){
+                if (v2.compareTo(BigDecimal.ZERO) == 0){ // "se o valor de 2 for igual a 0...
                     throw new IllegalArgumentException("pare imediatamente de fazer essa palhaçada");
+                    //para tudo para lançar exceção
                 }
                 yield v1.divide(v2, 6, RoundingMode.HALF_UP);
+                //yield é um RETURN, scale é o maximo de decimais da divisao
             }
             default -> throw new IllegalArgumentException("Xabu: " + operacao);
+            //É executado se a variável operacao não for igual a nenhum dos outros case
+            //("soma", "subtracao", "multiplicacao", "divisao").
 
         };
 
@@ -30,6 +36,7 @@ public class CalculadoraService {
 
     private BigDecimal toBigDecimal(String valor) {
         String aux = valor.trim().replace(",", ".");
+        //trim retira espaço em branco
         try {
             return new BigDecimal(aux);
         } catch (Exception e){
